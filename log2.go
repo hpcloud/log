c3533c8ac2b2320d53f30293e443f8abb5104586
Sridhar Ratnakumar <sridharr@activestate.com>
2012-11-19 12:59:52 -0800

    channel error/warn of logyard and related processes to cloud events
    do this by using the same logging prefix as the vcap logger:
    
      ERROR --
      WARN --

--- log2.go
+++ log2.go
@@ -14,72 +14,72 @@ type Logger struct {
 var std = &Logger{log.New(os.Stderr, "", log.LstdFlags)}
 
 func Info(v ...interface{}) {
-	std.Output(2, "[INFO] "+fmt.Sprint(v...))
+	std.Output(2, "INFO -- "+fmt.Sprint(v...))
 }
 
 func Infof(format string, v ...interface{}) {
-	std.Output(2, fmt.Sprintf("[INFO] "+format, v...))
+	std.Output(2, fmt.Sprintf("INFO -- "+format, v...))
 }
 
 func Warn(v ...interface{}) {
-	std.Output(2, "[WARN] "+fmt.Sprint(v...))
+	std.Output(2, "WARN -- "+fmt.Sprint(v...))
 }
 
 func Warnf(format string, v ...interface{}) {
-	std.Output(2, fmt.Sprintf("[WARN] "+format, v...))
+	std.Output(2, fmt.Sprintf("WARN -- "+format, v...))
 }
 
 func Error(v ...interface{}) {
-	std.Output(2, "[ERROR] "+fmt.Sprint(v...))
+	std.Output(2, "ERROR -- "+fmt.Sprint(v...))
 }
 
 func Errorf(format string, v ...interface{}) {
-	std.Output(2, fmt.Sprintf("[ERROR] "+format, v...))
+	std.Output(2, fmt.Sprintf("ERROR -- "+format, v...))
 }
 
 func Fatal(v ...interface{}) {
-	std.Output(2, "[FATAL] "+fmt.Sprint(v...)+"\n"+string(debug.Stack()))
+	std.Output(2, "FATAL -- "+fmt.Sprint(v...)+"\n"+string(debug.Stack()))
 	os.Exit(1)
 }
 
 func Fatalf(format string, v ...interface{}) {
-	std.Output(2, fmt.Sprintf("[FATAL] "+format, v...)+"\n"+string(debug.Stack()))
+	std.Output(2, fmt.Sprintf("FATAL -- "+format, v...)+"\n"+string(debug.Stack()))
 	os.Exit(1)
 }
 
 func (l *Logger) Info(v ...interface{}) {
-	l.Output(2, "[INFO] "+fmt.Sprint(v...))
+	l.Output(2, "INFO -- "+fmt.Sprint(v...))
 }
 
 func (l *Logger) Infof(format string, v ...interface{}) {
-	l.Output(2, fmt.Sprintf("[INFO] "+format, v...))
+	l.Output(2, fmt.Sprintf("INFO -- "+format, v...))
 }
 
 func (l *Logger) Warn(v ...interface{}) {
-	l.Output(2, "[WARN] "+fmt.Sprint(v...))
+	l.Output(2, "WARN -- "+fmt.Sprint(v...))
 }
 
 func (l *Logger) Warnf(format string, v ...interface{}) {
-	l.Output(2, fmt.Sprintf("[WARN] "+format, v...))
+	l.Output(2, fmt.Sprintf("WARN -- "+format, v...))
 }
 
 func (l *Logger) Error(v ...interface{}) {
-	l.Output(2, "[ERROR] "+fmt.Sprint(v...))
+	l.Output(2, "ERROR -- "+fmt.Sprint(v...))
 }
 
 func (l *Logger) Errorf(format string, v ...interface{}) {
-	l.Output(2, fmt.Sprintf("[ERROR] "+format, v...))
+	l.Output(2, fmt.Sprintf("ERROR -- "+format, v...))
 }
 
 // Fatal behaves mostly like log.Fatal but it also prints the current
 // goroutine's stacktrace.
 func (l *Logger) Fatal(v ...interface{}) {
-	l.Output(2, "[FATAL] "+fmt.Sprint(v...)+"\n"+string(debug.Stack()))
+	l.Output(2, "FATAL -- "+fmt.Sprint(v...)+"\n"+string(debug.Stack()))
 	os.Exit(1)
 }
 
 func (l *Logger) Fatalf(format string, v ...interface{}) {
-	l.Output(2, fmt.Sprintf("[FATAL] "+format, v...)+"\n"+string(debug.Stack()))
+	l.Output(2, fmt.Sprintf("FATAL -- "+format, v...)+"\n"+string(debug.Stack()))
 	os.Exit(1)
 }
 
